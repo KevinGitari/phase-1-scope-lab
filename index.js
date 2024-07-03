ccbae02902c95e215297324ae5aac708183394ab
KevinGitari <Kevin>
2024-07-03 22:43:24 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+// console.log(customerName)
+function uppercaseCustomerName(){
+   // console.log(customerName = customerName.touppercase() );
+   return customerName.toUpperCase
+}
+
+{var bestCustomer;
+ function setBestCustomer(){
+
+    }
+        bestcustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavouriteCustomer = 'some initial value'
+function changeLeastFavourite(){
+    leastFavouriteCustomer = 'some new value';
+}
+
+
